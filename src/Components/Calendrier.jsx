dae1e657ecc13e90d81f910cf5e7ccf133fe0ed6
Felix-Rheaume <1934266@cegeplimoilou.ca>
2023-09-12 16:27:02 -0400

    Ajout des components

---
+++ src/Components/Calendrier.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Calendrier extends React.Component{
+    render(){
+        return null;
+    }
+}
+
+export default Calendrier;
---
+++ src/Components/Convertisseur.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Convertisseur extends React.Component{
+    render(){
+        return null;
+    }
+}
+
+export default Convertisseur;
