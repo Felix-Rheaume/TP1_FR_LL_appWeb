f49d624b63efd11f22410e9d4709924737fbc359
Felix-Rheaume <1934266@cegeplimoilou.ca>
2023-09-15 13:51:51 -0400

    Correctif css du carousel

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,32 +1,37 @@
 import React from "react";
 import ReactCarousel from 'react-bootstrap/Carousel';
 import CarouselImage from './CarouselImage';
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 class Carousel extends React.Component {
     render() {
         return (
             <Container>
-                <div className="col-lg-10 col justify-content-lg-center">
-                    <h1>Carousel</h1>
-                    <ReactCarousel>
-                        <ReactCarousel.Item>
-                            <CarouselImage imageIndex={0} />
-                        </ReactCarousel.Item>
-                        <ReactCarousel.Item>
-                            <CarouselImage imageIndex={1} />
-                        </ReactCarousel.Item>
-                        <ReactCarousel.Item>
-                            <CarouselImage imageIndex={2} />
-                        </ReactCarousel.Item>
-                        <ReactCarousel.Item>
-                            <CarouselImage imageIndex={3} />
-                        </ReactCarousel.Item>
-                        <ReactCarousel.Item>
-                            <CarouselImage imageIndex={4} />
-                        </ReactCarousel.Item>
-                    </ReactCarousel>
-                </div>
+                <Row className="justify-content-md-center">
+                    <Col className="col-lg-10 col">
+
+                        <h1>Carousel</h1>
+                        <ReactCarousel>
+                            <ReactCarousel.Item>
+                                <CarouselImage imageIndex={0} />
+                            </ReactCarousel.Item>
+                            <ReactCarousel.Item>
+                                <CarouselImage imageIndex={1} />
+                            </ReactCarousel.Item>
+                            <ReactCarousel.Item>
+                                <CarouselImage imageIndex={2} />
+                            </ReactCarousel.Item>
+                            <ReactCarousel.Item>
+                                <CarouselImage imageIndex={3} />
+                            </ReactCarousel.Item>
+                            <ReactCarousel.Item>
+                                <CarouselImage imageIndex={4} />
+                            </ReactCarousel.Item>
+                        </ReactCarousel>
+
+                    </Col>
+                </Row>
+
             </Container>
 
         );
